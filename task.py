13ac85900a607c3f2bd74c6eb9635017cc1b64d9
Pooja Bhosale <bhosalepuja54@gmail.com>
2026-06-24 12:35:38 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,26 @@
+# find the second largest no[10,5, 20,8,20,5]
+# given sentence as part of basic NLP preprocessing task  "Hello!!! "world123", this is a TEST sentence ... AI@ML 2026!!!'
+#  replace none with mean of remaining elements
+
+# l = [10, 5, 20, 8, 20, 5]
+# largest = l[0]
+# for i in range(len(l)):
+#     if l[i] > largest:
+#         largest = l[i]
+# print("Largest element:", largest)
+
+
+# import re
+# sen="Hello!!! world123, this is a TEST sentence ... AI@ML 2026!!!"
+# S=sen.split()
+# print(S)
+# out=re.sub(r'[^\w\s]', '', str(S))
+# print(out)
+
+#  replace none with mean of remaining elements
+# s=[10,5,20,None,9]
+# Sum=sum(x for x in s if x is not None)
+# out=Sum/len([x for x in s if x is not None])
+# print(out)
+
+
